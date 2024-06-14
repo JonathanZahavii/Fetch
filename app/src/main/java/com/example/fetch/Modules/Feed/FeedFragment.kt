c768a21f0463fa69d38c932a657a2184348f4f75
jjonnyzz <xd.JZahavi@idf.il>
2024-06-14 15:00:32 +0300

    Basic android studio project. Navigation available. Feed and Profile basic fragments and layouts.

---
+++ app/src/main/java/com/example/fetch/Modules/Feed/FeedFragment.kt
@@ -0,0 +1,27 @@
+package com.example.fetch.Modules.Feed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fetch.databinding.FragmentFeedBinding
+
+class FeedFragment : Fragment() {
+
+    private var _binding: FragmentFeedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
